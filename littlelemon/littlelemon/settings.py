93f6e28a36cb0189ae8aa15156d64c36a336e86c
KolbieP <134655962+KolbieP@users.noreply.github.com>
2023-11-16 09:28:05 -0600

    Update settings.py

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'littlelemon',
         'USER': 'root',
-        'PASSWORD': 'sql23Installer',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
